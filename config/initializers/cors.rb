5e40e4d9f0660b32d306fa3920262c1946c8214f
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-22 23:13:21 -0400

    tweaked cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://salp-client.herokuapp.com/'
+    origins 'https://salp-client.herokuapp.com'
 
     resource '*',
       headers: :any,
